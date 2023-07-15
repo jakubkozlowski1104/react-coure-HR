f5f2b46f334d79f1ce840b3f14b89068040165a6
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-07-15 21:53:29 +0200

    add styles to root

--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -1,16 +1,8 @@
 import { GlobalStyle } from '../assets/styles/GlobalStyle.jsx';
 import UsersList from '../components/organisms/UsersList/UsersList.jsx';
-import styled, { ThemeProvider } from 'styled-components';
 import { theme } from '../assets/styles/Theme.jsx';
-
-const StyledWrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.lightGrey};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-`;
+import { StyledWrapper } from './Root.styles.jsx';
+import { ThemeProvider } from 'styled-components';
 
 const Root = () => (
   <ThemeProvider theme={theme}>
