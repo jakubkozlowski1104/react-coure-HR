a731b0c18ebcbdeaad879b69da5cb63e8259f5bd
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-08-04 21:54:06 +0200

    change false for initialState

--- src/components/organisms/Modal/useModal.jsx
+++ src/components/organisms/Modal/useModal.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Modal from './Modal';
 
 const useModal = (initialState = false) => {
-  const [isOpen, setModalState] = useState(false);
+  const [isOpen, setModalState] = useState(initialState);
 
   const handleOpenModal = () => setModalState(true);
   const handleCloseModal = () => setModalState(false);
