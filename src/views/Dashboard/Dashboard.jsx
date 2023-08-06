6f4ec894680dda01df9daa92bf426e684610511e
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-08-06 09:27:52 +0200

    delete Modal from useModal

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -6,13 +6,14 @@ import { useStudents } from '../../hooks/useStudents.jsx';
 import { useState, useEffect } from 'react';
 import useModal from '../../components/organisms/Modal/useModal.jsx';
 import StudentDetails from '../../components/molecules/StudentDetails/StudentDetails.jsx';
+import Modal from '../../components/organisms/Modal/Modal.jsx';
 
 const Dashboard = () => {
   const [groups, setGroups] = useState([]);
   const [currentStudent, setCurrentStudent] = useState(null);
   const { getGroups, getStudentById } = useStudents();
   const { id } = useParams();
-  const { Modal, isOpen, handleOpenModal, handleCloseModal } = useModal();
+  const { isOpen, handleOpenModal, handleCloseModal } = useModal();
 
   useEffect(() => {
     (async () => {
@@ -30,7 +31,7 @@ const Dashboard = () => {
   if (!id && groups.length > 0) return <Navigate to={`/group/${groups[0]}`} />;
 
   return (
-    <div>
+    <>
       <StyledGroupsWrapper>
         <p>Group {id}</p>
         {groups.map((group) => (
@@ -47,7 +48,7 @@ const Dashboard = () => {
           </Modal>
         ) : null}
       </ViewWrapper>
-    </div>
+    </>
   );
 };
 
